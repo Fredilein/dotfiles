4f724b03d446eec7de49f5baad6d832b4330183f
fredi <fredi@parrot.lab>
2021-05-04 16:15:53 -0400

    parrot bashrc and vim plug for neovim fix

--- install-scripts/install-minimal.sh
+++ install-scripts/install-minimal.sh
@@ -10,6 +10,7 @@ echo "## remove old config files";
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
 sudo rm -rf ~/.bashrc > /dev/null 2>&1
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+sudo rm -rf ~/.config/nvim/init.vim > /dev/null 2>&1
 
 echo "## install vim, tmux, curl";
 sudo apt-get install vim neovim tmux curl;
@@ -22,7 +23,7 @@ ln -s ~/.dotfiles/minimal/.tmux.conf ~;
 ln -s ~/.dotfiles/minimal/.bashrc ~;
 
 echo "## install vim-plug for neovim";
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 source ~/.bashrc;
