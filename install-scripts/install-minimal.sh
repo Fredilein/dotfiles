5f6af3055b6381c2f4b8049f76b514cb4c781f99
Fredilein <adi@ubuntu.com>
2021-05-04 12:50:02 -0700

    default to neovim config, fixed vim-plug installation

--- install-scripts/install-minimal.sh
+++ install-scripts/install-minimal.sh
@@ -12,14 +12,16 @@ sudo rm -rf ~/.bashrc > /dev/null 2>&1
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
 
 echo "## install vim, tmux, curl";
-sudo apt-get install vim tmux curl;
+sudo apt-get install vim neovim tmux curl;
 
 echo "## link vim & tmux conf";
 ln -s ~/.dotfiles/minimal/.vimrc ~;
+mkdir -p ~/.config/nvim/;
+ln -s ~/.dotfiles/minimal/.vimrc ~/.config/nvim/init.vim;
 ln -s ~/.dotfiles/minimal/.tmux.conf ~;
 ln -s ~/.dotfiles/minimal/.bashrc ~;
 
-echo "## install vim-plug for vim";
+echo "## install vim-plug for neovim";
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
