0b244d34cff825f1c3b755372364e4c5e6287d82
Fredilein <adi@ubuntu.com>
2021-05-07 09:59:59 -0700

    fix install script name

--- pretty/install-pretty.sh
+++ pretty/install-pretty.sh
@@ -12,6 +12,7 @@ sudo rm -rf $HOME/.bashrc > /dev/null 2>&1
 sudo rm -rf $HOME/.tmux.conf > /dev/null 2>&1
 sudo rm -rf $HOME/.alacritty.yml > /dev/null 2>&1
 sudo rm -rf $HOME/.config/nvim/init.vim > /dev/null 2>&1
+sudo rm -rf $HOME/.config/nvim/config > /dev/null 2>&1
 
 echo "## install vim, tmux, curl";
 # sudo apt-get install neovim tmux curl;
@@ -23,8 +24,9 @@ ln -s $HOME/.dotfiles/pretty/init.vim $HOME/.config/nvim/init.vim;
 ln -s $HOME/.dotfiles/pretty/config/ $HOME/.config/nvim/config/;
 ln -s $HOME/.dotfiles/pretty/.tmux.conf $HOME;
 ln -s $HOME/.dotfiles/pretty/.bashrc $HOME;
+ln -s $HOME/.dotfiles/pretty/.zshrc $HOME;
 ln -s $HOME/.dotfiles/pretty/.alacritty.yml $HOME;
 
 echo "## install vim-plug for neovim";
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       # https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
