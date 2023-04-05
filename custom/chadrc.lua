4bc15dceb13d984865ebc190d9552eaa8e3446b2
Adrian Zanga <adrian.zanga@icloud.com>
2023-04-05 22:30:23 +0200

    tokyonight theme

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -5,11 +5,15 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "one_light" },
+  theme = "tokyonight",
+  theme_toggle = { "tokyonight", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  statusline = {
+    separator_style = "round"
+  }
 }
 
 M.plugins = "custom.plugins"
